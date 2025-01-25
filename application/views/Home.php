4b0c50ed53ccb61b05d5b4912d157bf53a7a8f32
VonBryan <vonbanalbryan18v@gmail.com>
2025-01-25 20:23:44 +0800

    Test Login.

--- application/views/Home.php
+++ application/views/Home.php
@@ -6,12 +6,31 @@
     <title>Sign In Form</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            background: url("https://img.freepik.com/free-vector/white-abstract-wallpaper_23-2148830028.jpg");
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .card {
+            padding: 80px;
+            box-shadow: 0px 0px 10px 1px black;
+        }
+
+        .btn {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
+    <div class="container mt-5 main_container">
+        <div class="row justify-content-center row_con">
             <div class="col-md-6">
-                <div class="card shadow-lg">
+                <div class="card">
+                    
                     <div class="card-body">
                         <h3 class="card-title text-center">Sign In</h3>
                         <form>
